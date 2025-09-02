24e2916fcf684fd65cbe68e8ee5ba0964307a3b9
zzz1275 <zhang3310028@gmail.com>
2025-09-02 11:26:10 +0800

    0902

--- .vitepress/theme/components/LastPosts.vue
+++ .vitepress/theme/components/LastPosts.vue
@@ -24,10 +24,30 @@ function changePage(newPage) {
   }
 }
 
-// 格式化日期，让它好看点
+Date.prototype.format = function(fmt) { 
+     var o = { 
+        "M+" : this.getMonth()+1,                 //月份 
+        "d+" : this.getDate(),                    //日 
+        "h+" : this.getHours(),                   //小时 
+        "m+" : this.getMinutes(),                 //分 
+        "s+" : this.getSeconds(),                 //秒 
+        "q+" : Math.floor((this.getMonth()+3)/3), //季度 
+        "S"  : this.getMilliseconds()             //毫秒 
+    }; 
+    if(/(y+)/.test(fmt)) {
+            fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length)); 
+    }
+     for(var k in o) {
+        if(new RegExp("("+ k +")").test(fmt)){
+             fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+         }
+     }
+    return fmt; 
+}
+
 function formatDate(date) {
   if (!(date instanceof Date)) date = new Date(date);
-  return date.toLocaleDateString(); // 或者用更详细的格式
+  return date.format("yyyy-MM-dd hh:mm:ss"); 
 }
 </script>
 
